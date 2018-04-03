f30738814d65a0c6c43a4aa44cc6ae02affbf585
CloudRex <cloudrex@outlook.com>
2018-04-03 18:48:23 -0400

    Updated about command with link to inviting the bot

--- src/data/commands/about.js
+++ src/data/commands/about.js
@@ -3,6 +3,7 @@ import AccessLevelType from "../../core/access-level-type";
 
 const command = new Command("about", "Displays information about the bot", [], null, 0, AccessLevelType.Guest, (context) => {
 	context.respond({
+		"Adopt me": "To add Tux to your server, click [here](https://discordapp.com/oauth2/authorize?client_id=381949722157514752&scope=bot)",
 		Description: "Tux is a tiny Discord bot created with flexibility in mind.",
 		"Command Trigger": context.bot.settings.general.commandTrigger,
 		Version: context.bot.settings.general.version
