88c02457d0886175041968e3f43ab34d4321118d
CloudRex <cloudrex@outlook.com>
2018-04-03 00:51:57 -0400

    Updated version command

--- src/data/commands/version.js
+++ src/data/commands/version.js
@@ -1,7 +1,7 @@
 import Command from "../../commands/command";
 
 const command = new Command("version", "View the bot's version", ["ver"], null, 0, [], (context) => {
-	context.respond(`\`Version: ${context.bot.settings.general.version}\``);
+	context.respond(`Version: \`${context.bot.settings.general.version}`);
 }, () => true);
 
 export default command;
