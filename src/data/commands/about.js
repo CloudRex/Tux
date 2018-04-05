c4c5184751f2a74be68eff12c21cfd685628b20f
CloudRex <cloudrex@outlook.com>
2018-04-05 00:11:09 -0400

    Updated about command

--- src/data/commands/about.js
+++ src/data/commands/about.js
@@ -2,11 +2,21 @@ import AccessLevelType from "../../core/access-level-type";
 
 export default {
 	executed(context) {
+		const guilds = context.bot.client.guilds.array();
+
+		let users = 0;
+
+		for (let i = 0; i < guilds.length; i++) {
+			users += guilds[i].memberCount;
+		}
+
 		context.respond({
 			"Adopt me": "To add Tux to your server, click [here](https://discordapp.com/oauth2/authorize?client_id=381949722157514752&scope=bot)",
 			Description: "Tux is a tiny Discord bot created with flexibility in mind.",
 			"Command Trigger": context.bot.settings.general.commandTrigger,
-			Version: context.bot.settings.general.version
+			Version: context.bot.settings.general.version,
+			Guilds: context.bot.client.guilds.size,
+			Users: users
 		});
 	},
 
