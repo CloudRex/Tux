0a331e21e79f762e03c3b7eafa4201fedf9706b7
CloudRex <cloudrex@outlook.com>
2018-04-05 23:22:41 -0400

    Fixed bugs with treasure finder

--- src/data/features/treasure-finder.js
+++ src/data/features/treasure-finder.js
@@ -43,13 +43,23 @@ export default class TreasureFinder extends Feature {
 		return true;
 	}
 
+	getWaitingIndex(userId) {
+		for (let i = 0; i < this.waiting.length; i++) {
+			if (this.waiting[i].id === userId.toString()) {
+				return i;
+			}
+		}
+
+		return null;
+	}
+
 	enabled(bot) {
 		this.waiting = [];
 
 		bot.client.on("message", async (message) => {
 			if (!message.author.bot) {
 				if (message.author.id !== bot.client.user.id) {
-					const treasure = this.treasures[Utils.getRandomInt(0, this.treasures.length - 1)];
+					const treasure = this.treasures[0];
 
 					if (Utils.getRandomInt(0, treasure.value) === 0) {
 						console.log(`${message.author.username}@${message.guild.name} found a ${treasure.name}`);
@@ -58,24 +68,33 @@ export default class TreasureFinder extends Feature {
 						});
 
 						if (msg) {
-							msg.delete(4000);
+							msg.delete(5000);
 							msg.react("🖐");
-							this.waiting.push(message.author.id);
+
+							this.waiting.push({
+								id: message.author.id.toString(),
+								treasure: treasure
+							});
 						}
 					}
 				}
 			}
 		});
 
-		bot.client.on("messageReactionAdd", (reaction, user) => {
+		bot.client.on("messageReactionAdd", async (reaction, user) => {
 			if (reaction.emoji.name === "🖐") {
-				if (this.waiting.includes(user.id)) {
+				const index = this.getWaitingIndex(user.id);
+
+				if (index !== null) {
+					const { treasure } = this.waiting[index];
+					const points = await bot.database.addUserPoints(user.id, treasure.value);
+
+					this.waiting.splice(index, 1);
 					reaction.message.clearReactions();
-					reaction.message.edit("You've obtained **50** points!");
-					reaction.message.delete(4000);
-					this.waiting.splice(this.waiting.indexOf(user.id), 1);
-					bot.database.addUserPoints(user.id, 50);
-					console.log(`${user.username}@${reaction.message.guild.name} caught the penguin`);
+					reaction.message.edit(`You've obtained **${treasure.value}** points and now have a total of **${points}** points`);
+					reaction.message.delete(5000);
+
+					console.log(`${user.username}@${reaction.message.guild.name} caught a ${treasure.name} (${treasure.value})`);
 				}
 			}
 		});
