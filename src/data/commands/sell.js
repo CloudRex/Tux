2135ffa54fbcfdc9a6e855cbe786b1c6ed2aba4c
CloudRex <cloudrex@outlook.com>
2018-04-08 13:40:41 -0400

    Updated sell command to support emojis as first argument

--- src/data/commands/sell.js
+++ src/data/commands/sell.js
@@ -3,14 +3,20 @@ import AccessLevelType from "../../core/access-level-type";
 export default {
 	async executed(context) {
 		if (context.arguments.length === 1) {
-			if (context.arguments[0] === "*") {
+			let itemKey = context.arguments[0];
+
+			if (itemKey.includes(":")) {
+				itemKey = itemKey.replace(":", "").replace(":", "");
+			}
+
+			if (itemKey === "*" || itemKey === "all") {
 				// TODO
 			}
 			else {
-				const item = await context.bot.database.getItem(context.message.author.id, context.arguments[0]);
+				const item = await context.bot.database.getItem(context.message.author.id, itemKey);
 
 				if (item) {
-					await context.bot.database.removeItem(context.message.author.id, context.arguments[0]);
+					await context.bot.database.removeItem(context.message.author.id, itemKey);
 
 					const points = await context.bot.database.addUserPoints(context.message.author.id, item.value);
 
