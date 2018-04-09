3cd39ba1f49a616331ea8a82de042e60fa0e0e52
CloudRex <cloudrex@outlook.com>
2018-04-08 22:21:41 -0400

    Added ban and unban command foundations

--- src/data/commands/ban.js
+++ src/data/commands/ban.js
@@ -2,8 +2,19 @@ import AccessLevelType from "../../core/access-level-type";
 
 export default {
 	executed(context) {
-		if (context.arguments.length === 1) {
-			context.respond("Command not yet fully implemented");
+		if (context.arguments.length === 2) {
+			const { username } = context.bot.client.users.find("id", context.arguments[0]);
+
+			context.message.guild.ban(context.arguments[0], {
+				reason: context.arguments[1]
+			})
+				.then(() => context.respond(`And just like that, **${username}** was long gone for \`${context.arguments[1]}\`.`, "", "GREEN"))
+				.catch((error) => {
+					context.respond(`Operation failed to complete. (${error.message})`, "", "RED");
+				});
+		}
+		else {
+			context.respond("Invalid amount of arguments.");
 		}
 	},
 
@@ -16,6 +27,6 @@ export default {
 		description: "Bans a member from the server",
 		accessLevel: AccessLevelType.Admin,
 		aliases: [],
-		maxArguments: 1
+		maxArguments: 2
 	}
 };
---
+++ src/data/commands/unban.js
@@ -0,0 +1,30 @@
+import AccessLevelType from "../../core/access-level-type";
+
+export default {
+	executed(context) {
+		if (context.arguments.length === 2) {
+			// const { username } = context.bot.client.users.find("id", context.arguments[0]);
+
+			context.message.guild.ban(context.arguments[0], context.arguments[1])
+				.then(() => context.respond(`And just like that, **todo** was unbaned for \`${context.arguments[1]}\`.`, "", "GREEN"))
+				.catch((error) => {
+				context.respond(`Operation failed to complete. (${error.message})`, "", "RED");
+			});
+		}
+		else {
+			context.respond("Invalid amount of arguments.");
+		}
+	},
+
+	canExecute(context) {
+		return true;
+	},
+
+	meta: {
+		name: "unban",
+		description: "Unbans a member from the server",
+		accessLevel: AccessLevelType.Admin,
+		aliases: ["uban"],
+		maxArguments: 2
+	}
+};
