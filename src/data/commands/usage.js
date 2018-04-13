21f835a01c69e0b11332d10549c2ebec0d6bc6a5
CloudRex <cloudrex@outlook.com>
2018-04-13 17:33:05 -0400

    Added usage command foundation

---
+++ src/data/commands/usage.js
@@ -0,0 +1,22 @@
+import AccessLevelType from "../../core/access-level-type";
+import CommandCategoryType from "../../commands/command-category-type";
+
+export default {
+	executed(context) {
+		context.respond("Command not yet implemented.", "", "RED");
+	},
+
+	canExecute(context) {
+		return true;
+	},
+
+	meta: {
+		name: "usage",
+		description: "View the usage of a command",
+		accessLevel: AccessLevelType.Guest,
+		aliases: ["usg"],
+		maxArguments: 1,
+		args: {},
+		category: CommandCategoryType.General
+	}
+};
