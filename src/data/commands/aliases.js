43b06aa5e685af5e88fb5b7cf854e97148291bf2
CloudRex <cloudrex@outlook.com>
2018-04-13 17:31:33 -0400

    Added aliases command foundation

---
+++ src/data/commands/aliases.js
@@ -0,0 +1,22 @@
+import AccessLevelType from "../../core/access-level-type";
+import CommandCategoryType from "../../commands/command-category-type";
+
+export default {
+	executed(context) {
+		context.respond("Command not yet implemented.", "", "RED");
+	},
+
+	canExecute(context) {
+		return true;
+	},
+
+	meta: {
+		name: "aliases",
+		description: "View the aliases of a command",
+		accessLevel: AccessLevelType.Guest,
+		aliases: ["alias"],
+		maxArguments: 1,
+		args: {},
+		category: CommandCategoryType.General
+	}
+};
