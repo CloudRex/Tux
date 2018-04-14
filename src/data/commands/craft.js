c0931f62e742b480ffb0859334f60f8b53da61ef
CloudRex <cloudrex@outlook.com>
2018-04-13 21:22:07 -0400

    Added craft and lockdown command foundations

---
+++ src/data/commands/craft.js
@@ -0,0 +1,28 @@
+import AccessLevelType from "../../core/access-level-type";
+import CommandCategoryType from "../../commands/command-category-type";
+
+export default {
+	executed(context) {
+		if (context.arguments.length === 2) {
+			// TODO
+		}
+		else {
+			context.respond("Invalid amount of arguments, expecting 2.", "", "RED");
+		}
+	},
+
+	canExecute(context) {
+		return true;
+	},
+
+	meta: {
+		name: "craft",
+		description: "Craft an item using other items",
+		accessLevel: AccessLevelType.Member,
+		aliases: [],
+		maxArguments: 2,
+		args: {},
+		category: CommandCategoryType.Economy,
+		enabled: true
+	}
+};
---
+++ src/data/commands/lockdown.js
@@ -0,0 +1,25 @@
+import AccessLevelType from "../../core/access-level-type";
+import CommandCategoryType from "../../commands/command-category-type";
+
+export default {
+	executed(context) {
+		context.respond("Initiated server lockdown mode.");
+
+		// TODO
+	},
+
+	canExecute(context) {
+		return true;
+	},
+
+	meta: {
+		name: "lockdown",
+		description: "Craft an item using other items",
+		accessLevel: AccessLevelType.Admin,
+		aliases: [],
+		maxArguments: 1,
+		args: {},
+		category: CommandCategoryType.Moderation,
+		enabled: true
+	}
+};
