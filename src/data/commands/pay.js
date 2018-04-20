d9c6a8e14ce6b89d8700159ea089ce95d215d216
CloudRex <cloudrex@outlook.com>
2018-04-20 14:34:12 -0400

    Added default user config and updated pay command

--- src/data/commands/pay.js
+++ src/data/commands/pay.js
@@ -5,8 +5,23 @@ export default {
 	treasures: null,
 
 	async executed(context) {
-		// TODO
-		context.respond("Command not yet implemented");
+		const senderBalance = await context.bot.database.getUserPoints(context.sender.id);
+		const recipient = context.message.mentions.users.array()[0];
+		const amount = parseInt(context.arguments[1]);
+
+		if (amount > 0) {
+			if (senderBalance >= amount) {
+				await context.bot.database.addUserPoints(context.sender.id, -amount);
+				await context.bot.database.addUserPoints(recipient.id, amount);
+				context.respond(`Successfully payed **${amount}** coins to **${recipient.username}**.`, "", "GREEN");
+			}
+			else {
+				context.respond("You don't have enough coins.", "", "RED");
+			}
+		}
+		else {
+			context.respond("You can't pay that amount.", "", "RED");
+		}
 	},
 
 	canExecute(context) {
@@ -18,10 +33,11 @@ export default {
 		description: "Pay another player",
 		accessLevel: AccessLevelType.Member,
 		aliases: [],
-		maxArguments: 1,
+		maxArguments: 2,
 
 		args: {
-			item: "!:user"
+			recipient: "!:user",
+			amount: "!number"
 		},
 
 		category: CommandCategoryType.Economy,
--- src/data/commands/say.js
+++ src/data/commands/say.js
@@ -3,6 +3,12 @@ import CommandCategoryType from "../../commands/command-category-type";
 
 export default {
 	async executed(context) {
+		if (context.arguments[0].toLowerCase() === "beep boop") {
+			context.respond("No.", "", "RED");
+
+			return;
+		}
+
 		context.respond(context.arguments[0], "", "GREEN");
 	},
 
