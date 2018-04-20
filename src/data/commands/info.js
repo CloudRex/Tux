89783d4962dbb1ba123a84520823b8c7b9362402
CloudRex <cloudrex@outlook.com>
2018-04-20 18:38:23 -0400

    Added score section to the info command

--- src/data/commands/info.js
+++ src/data/commands/info.js
@@ -4,6 +4,7 @@ import CommandCategoryType from "../../commands/command-category-type";
 export default {
 	executed(context) {
 		const { guild } = context.message;
+		const computeScore = (g) => ((g.memberCount / 100) + g.emojis.array().length + g.roles.array().length);
 
 		context.respond({
 			Name: guild.name,
@@ -14,8 +15,9 @@ export default {
 			Region: guild.region,
 			"Default Channel": guild.defaultChannel ? guild.defaultChannel : "None",
 			"Custom Emojis": guild.emojis.array().length,
-			"Created At": guild.createdAt
-		}, "", "", guild.iconURL);
+			"Created At": guild.createdAt,
+			Score: `:star: ${computeScore(guild)}`
+		}, "", "GREEN", guild.iconURL);
 	},
 
 	canExecute(context) {
