595fefde6c6e26379ad138e239fa72a0c3602db2
CloudRex <cloudrex@outlook.com>
2018-04-21 20:57:29 -0400

    Updated config set response with check emoji

--- src/data/commands/config.js
+++ src/data/commands/config.js
@@ -28,7 +28,7 @@ export default {
 				value = (value === "true" ? true : (value === "false" ? false : (Number.isNaN(value) ? value : parseInt(value))));
 
 				context.arguments[0].startsWith("global") ? context.bot.userConfig.set(context.arguments[0], value) : context.bot.userConfig.setLocal(context.message.guild.id, context.arguments[0], value);
-				context.ok(`Set **${context.arguments[0]}** to **${context.arguments[1]}**`);
+				context.ok(`<:tuxcheck:436998015652462603> Set **${context.arguments[0]}** to **${context.arguments[1]}**`);
 			}
 			else {
 				context.fail("Property is not pre-defined and therefore not configurable.");
