c47da7b232dd9959a6c9befcd56ef256328f9e5f
Allen Lantz <allen.m.lantz@gmail.com>
2018-04-23 08:34:38 -0500

    Make resolveId resolve roles and channels

--- src/core/utils.js
+++ src/core/utils.js
@@ -19,11 +19,17 @@ export default class Utils {
 	}
 
 	/**
-	 * @param {string} userMention
+	 * @param {string} mention
 	 * @returns {string}
 	 */
-	static resolveId(userMention) {
-		return userMention.replace("<", "").replace(">", "").replace("@", "").replace("!", "");
+	static resolveId(mention) {
+		return mention
+			.replace("<", "")
+			.replace(">", "")
+			.replace("@", "")
+			.replace("!", "")
+			.replace("&", "")
+			.replace("#", "");
 	}
 
 	/**
