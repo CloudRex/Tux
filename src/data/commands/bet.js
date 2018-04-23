e78dbcae372143fa2b1fe2b12c8ba21f5e766677
Allen Lantz <allen.m.lantz@gmail.com>
2018-04-23 11:29:01 -0500

    Fix bet command

--- src/data/commands/bet.js
+++ src/data/commands/bet.js
@@ -1,26 +1,27 @@
 import AccessLevelType from "../../core/access-level-type";
 import CommandCategoryType from "../../commands/command-category-type";
+import Utils from "../../core/utils";
 
 export default {
-	executed(context) {
-		let amount = context.arguments.length === 0 ? 50 : parseInt(context.arguments[0]);
-		
+	async executed(context) {
+		const amount = context.arguments.length === 0 ? 50 : parseInt(context.arguments[0]);
+
 		if (amount < 50) {
 			context.fail("Minimum bet is :small_orange_diamond:50 or higher.");
 		}
 		else {
 			const coins = await context.bot.database.getUserPoints(context.sender.id);
-			
+
 			if (coins >= amount) {
 				const win = Utils.getRandomInt(0, 1);
-				
+
 				if (win === 1) {
 					await context.bot.database.addUserPoints(context.sender.id, amount);
 					context.ok(`**You won!** You've acquired :small_orange_diamond:**${amount}**.`);
 				}
 				else {
 					await context.bot.database.addUserPoints(context.sender.id, amount);
-					
+
 					// TODO: Shouldn't be fail. (it auto deletes)
 					context.fail(`**Too bad!** You've lost :small_orange_diamond:**${amount}**.`);
 				}
@@ -41,11 +42,11 @@ export default {
 		accessLevel: AccessLevelType.Member,
 		aliases: [],
 		maxArguments: 1,
-		
+
 		args: {
 			amount: ":positiveNumber"
 		},
-		
+
 		category: CommandCategoryType.Fun,
 		enabled: true
 	}
