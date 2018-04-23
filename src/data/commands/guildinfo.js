12a69a8f6ae121442c1b20b5962bfc04c0a0110c
CloudRex <cloudrex@outlook.com>
2018-04-22 23:01:10 -0400

    Added guildinfo command

--- src/data/commands/guildinfo.js
+++ src/data/commands/guildinfo.js
@@ -1,22 +1,40 @@
 import AccessLevelType from "../../core/access-level-type";
 import CommandCategoryType from "../../commands/command-category-type";
 import EmbedBuilder from "../../core/embed-builder";
+import MessageBuilder from "../../core/message-builder";
 
 export default {
 	async executed(context) {
-		const guild = context.bot.client.guilds.get(context.arguments[0]);
-		const invites = await guild.fetchInvites();
+		if (context.arguments.length === 1) {
+			const guild = context.bot.client.guilds.get(context.arguments[0]);
+			const invites = await guild.fetchInvites();
 
-		context.message.channel.send(new EmbedBuilder()
-			.title(`Information of ${guild.name}`)
-			.field("Owner", guild.owner.user.tag)
-			.field("Members", guild.memberCount)
-			.field("Created At", guild.createdAt.toString())
-			.field("Joined At", guild.joinedAt.toString())
-			.field("Join", invites ? `[Click to Join](${invites.array()[0].url})` : "No invites.")
-			.color("GREEN")
-			.thumbnail(guild.iconURL)
-			.build());
+			context.message.channel.send(new EmbedBuilder()
+				.title(`Information of ${guild.name}`)
+				.field("Owner", guild.owner.user.tag)
+				.field("Members", guild.memberCount)
+				.field("Created At", guild.createdAt.toString())
+				.field("Joined At", guild.joinedAt.toString())
+				.field("Join", invites ? `[Click to Join](${invites.array()[0].url})` : "No invites.")
+				.color("GREEN")
+				.thumbnail(guild.iconURL)
+				.build());
+		}
+		else {
+			const guilds = context.bot.client.guilds.array();
+			const message = new MessageBuilder();
+
+			const result = new EmbedBuilder()
+				.color("GREEN")
+				.title(`Guilds (${guilds.length})`);
+
+			for (let i = 0; i < guilds.length; i++) {
+				message.add(`${guilds[i].name} (\`${guilds[i].id}\`)`).line();
+			}
+
+			result.text(message.build());
+			context.message.channel.send(result.build());
+		}
 	},
 
 	canExecute(context) {
