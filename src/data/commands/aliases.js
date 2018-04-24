879832aa70ee23be9462131af6236c929c5ed30b
CloudRex <cloudrex@outlook.com>
2018-04-23 21:43:40 -0400

    Finished aliases command

--- src/data/commands/aliases.js
+++ src/data/commands/aliases.js
@@ -1,9 +1,35 @@
 import AccessLevelType from "../../core/access-level-type";
 import CommandCategoryType from "../../commands/command-category-type";
+import MessageBuilder from "../../core/message-builder";
 
 export default {
 	executed(context) {
-		context.fail("Command not yet implemented.");
+		const command = context.bot.commands.getByBase(context.arguments[0]);
+
+		if (!command) {
+			context.fail("Invalid command base name.");
+
+			return;
+		}
+
+		const response = new MessageBuilder();
+
+		if (command.aliases.length > 0) {
+			for (let i = 0; i < command.aliases.length; i++) {
+				response.add(command.aliases[i]);
+
+				if (i !== command.aliases.length - 1) {
+					response.add(", ");
+				}
+			}
+		}
+		else {
+			response.add("This command has no aliases.");
+		}
+
+		context.sections({
+			Aliases: response.build()
+		});
 	},
 
 	canExecute(context) {
@@ -16,7 +42,11 @@ export default {
 		accessLevel: AccessLevelType.Guest,
 		aliases: ["alias"],
 		maxArguments: 1,
-		args: {},
+
+		args: {
+			command: "!string"
+		},
+
 		category: CommandCategoryType.General,
 		enabled: true
 	}
