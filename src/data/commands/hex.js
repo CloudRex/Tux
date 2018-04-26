9993b70dbc9741e4fc8f462854168fdbb2dd0f06
CloudRex <cloudrex@outlook.com>
2018-04-26 01:19:36 -0400

    Added hex command, gold coins and silver coins emojis

--- src/data/commands/hex.js
+++ src/data/commands/hex.js
@@ -1,9 +1,12 @@
 import AccessLevelType from "../../core/access-level-type";
 import CommandCategoryType from "../../commands/command-category-type";
+import MessageBuilder from "../../core/message-builder";
+
+const generate = () => `#${(Math.random() * 0xFFFFFF << 0).toString(16)}`;
 
 export default {
 	executed(context) {
-
+		context.ok(new MessageBuilder().codeBlock("css", generate()).build());
 	},
 
 	canExecute(context) {
