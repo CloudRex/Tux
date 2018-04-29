41c48f95ab6b8e0498e89ca3c678976331352238
CloudRex <cloudrex@outlook.com>
2018-04-28 23:38:54 -0400

    Updated insult command's insults

--- src/data/commands/insult.js
+++ src/data/commands/insult.js
@@ -3,12 +3,14 @@ import CommandCategoryType from "../../commands/command-category-type";
 import Utils from "../../core/utils";
 
 const insults = [
-	"{subject} has officially become a piece of poo.",
+	"{subject} has magically turned a pile of poo.",
 	"{subject} is now known as no-one.",
 	"{subject} now works cleaning toilets.",
 	"{subject} needs to be reborn.",
 	"{subject}'s father was Hitler.",
-	"{subject} no u."
+	"{subject} no u.",
+	"{subject} now works as a clown full-time.",
+	"{subject} lives in the sewers."
 ];
 
 const fillInsult = (subject, insult) => insult.replace("{subject}", `**${subject}**`);
