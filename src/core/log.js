7629fc9209817bf2531eab25bf2f4ef7389dec67
Allen Lantz <allen.m.lantz@gmail.com>
2018-04-29 23:44:32 -0500

    Log file

--- src/core/log.js
+++ src/core/log.js
@@ -1,49 +1,54 @@
 const colors = require("colors");
+const fs = require("fs");
 
 export default class Log {
     // Static methods
-    static log(message, color = 'white', prefix = null) {
-        const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-        message = `[${date}] ${colors[color](message)}`;
+    static async log(message, color = "white", prefix = null) {
+        const date = new Date().toISOString().replace(/T/, " ").replace(/\..+/, "");
+
+        process.stdout.write(`\x1B[2D[${date}] ${colors[color](message)}\n> `);
+
         if (prefix !== null) {
-            message = `${prefix} ${message}`;
+            message = `<${prefix.toUpperCase()}> ${message}`;
         }
-        process.stdout.write(`${message}\n`);
+        fs.writeFile("bot.log", `[${date}] ${message}\n`, { flag: "a" }, (err) => {
+            if (err) throw err;
+        });
     }
 
     static info(message) {
-        Log.log(message, 'cyan');
+        Log.log(message, "cyan", "info");
     }
 
     static success(message) {
-        Log.log(message, 'green');
+        Log.log(message, "green", "sucs");
     }
 
     static warn(message) {
-        Log.log(message, 'yellow');
+        Log.log(message, "yellow", "warn");
     }
 
     static error(message) {
-        Log.log(message, 'red');
+        Log.log(message, "red", "dang");
     }
 
     static verbose(message) {
         if (Log.instance !== null) {
             if (Log.instance.verbose_mode) {
-                Log.log(message, 'grey');
+                Log.log(message, "grey");
             }
         } else {
-            Log.log(message, 'grey');
+            Log.log(message, "grey");
         }
     }
 
     static debug(message) {
         if (Log.instance !== null) {
             if (Log.instance.debug_mode) {
-                Log.log(message, 'magenta');
+                Log.log(message, "magenta", "dbug");
             }
         } else {
-            Log.log(message, 'magenta');
+            Log.log(message, "magenta", "dbug");
         }
     }
 
@@ -54,35 +59,35 @@ export default class Log {
         Log.instance = this;
     }
 
-    log(message, color = 'white', prefix = null, throwMsg = false) {
+    log(message, color = "white", prefix = null, throwMsg = false) {
         Log.log(message, color, prefix, throwMsg);
     }
 
     info(message) {
-        this.log(message, 'cyan');
+        this.log(message, "cyan", "info");
     }
 
     success(message) {
-        this.log(message, 'green');
+        this.log(message, "green", "sucs");
     }
 
     warn(message) {
-        this.log(message, 'yellow');
+        this.log(message, "yellow", "warn");
     }
 
     error(message) {
-        this.log(message, 'red');
+        this.log(message, "red", "dang");
     }
 
     verbose(message) {
         if (this.verbose_mode) {
-            this.log(message, 'gray');
+            this.log(message, "gray");
         }
     }
 
     debug(message) {
         if (this.debug_mode) {
-            this.log(message, 'magenta');
+            this.log(message, "magenta", "dbug");
         }
     }
 
