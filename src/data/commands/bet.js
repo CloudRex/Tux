a5ff5da6edd9ca9d559caf81db5b518a45527cb5
CloudRex <cloudrex@outlook.com>
2018-04-30 16:15:53 -0400

    Fixed bet command

--- src/data/commands/bet.js
+++ src/data/commands/bet.js
@@ -13,17 +13,17 @@ export default {
 			const coins = await context.bot.database.getUserPoints(context.sender.id);
 
 			if (coins >= amount) {
-				const win = Utils.getRandomInt(0, 1);
+				const win = Utils.getRandomInt(0, 2);
 
 				if (win === 1) {
 					await context.bot.database.addUserPoints(context.sender.id, amount);
-					context.ok(`**You won!** You've acquired :small_orange_diamond:**${amount}**.`);
+					context.ok(`**You won!** You've acquired${context.emojis.get("silver-coin")}**${amount}**.`);
 				}
 				else {
-					await context.bot.database.addUserPoints(context.sender.id, amount);
+					await context.bot.database.addUserPoints(context.sender.id, -amount);
 
 					// TODO: Shouldn't be fail. (it auto deletes)
-					context.fail(`**Too bad!** You've lost :small_orange_diamond:**${amount}**.`);
+					context.fail(`**Too bad!** You've lost${context.emojis.get("silver-coin")}**${amount}**.`);
 				}
 			}
 			else {
